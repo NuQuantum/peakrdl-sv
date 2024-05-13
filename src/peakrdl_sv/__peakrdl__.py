ed46c267dbd71663e45a841c14e53eb7234fd9f0
Jonah Foley <jonah.foley@nu-quantum.com>
2024-05-13 15:54:44 +0100

    dev: updates the naming

--- src/peakrdl_sv/__peakrdl__.py
+++ src/peakrdl_sv/__peakrdl__.py
@@ -5,39 +5,38 @@ import argparse
 from peakrdl.plugins.exporter import ExporterSubcommandPlugin
 from systemrdl.node import AddrmapNode
 
-from .exporter import VerilogExporter
+from .exporter import PythonExporterBase
+from .exporter import VerilogExporterBase
 
 
-def validate_reset_polarity(string):
-    if string not in ("high", "low"):
-        raise argparse.ArgumentError
-    return string
-
-
-def validate_reset_type(string):
-    if string not in ("async", "sync"):
-        raise argparse.ArgumentError
-    return string
-
-
-class Exporter(ExporterSubcommandPlugin):
+class VerilogExporter(ExporterSubcommandPlugin):
     short_desc = "A SystemVerilog SystemRDL exporter"
 
     def add_exporter_arguments(self, arg_group: argparse.ArgumentParser) -> None:
         arg_group.add_argument(
             "--reset-polarity",
             default="high",
-            type=validate_reset_polarity,
+            type=str,
+            choices=["high", "low"],
             help="Set the reset polarity",
         )
 
         arg_group.add_argument(
             "--reset-type",
             default="sync",
-            type=validate_reset_type,
+            type=str,
+            choices=["async", "sync"],
             help="set the reset type",
         )
 
     def do_export(self, top_node: AddrmapNode, options: argparse.Namespace) -> None:
-        exporter = VerilogExporter()
+        exporter = VerilogExporterBase()
+        exporter.export(top_node, options)
+
+
+class PythonExporter(ExporterSubcommandPlugin):
+    short_desc = "A Python register map SystemRDL exporter"
+
+    def do_export(self, top_node: AddrmapNode, options: argparse.Namespace) -> None:
+        exporter = PythonExporterBase()
         exporter.export(top_node, options)
--- src/peakrdl_sv/exporter.py
+++ src/peakrdl_sv/exporter.py
@@ -12,7 +12,7 @@ from systemrdl.node import RootNode
 from peakrdl_sv.listener import Listener
 
 
-class VerilogExporter:
+class VerilogExporterBase:
     def __init__(self):
         self.listener = Listener()
 
@@ -50,3 +50,36 @@ class VerilogExporter:
         )
         with reg_pkg_path.open("w") as f:
             f.write(reg_pkg_tpl.render(block=self.listener.top_node))
+
+
+class PythonExporterBase:
+    def __init__(self):
+        self.listener = Listener()
+
+    def walk(self, node: RootNode | AddrmapNode) -> None:
+        if isinstance(node, RootNode):
+            node = node.top
+
+        self.listener.walk(node, unroll=True)
+
+    def export(
+        self,
+        node: RootNode | AddrmapNode | None,
+        options: argparse.Namespace,
+    ) -> None:
+        if node:
+            self.walk(node)
+        elif not self.top:
+            raise RuntimeError
+
+        outpath = Path(options.output)
+        if not outpath.exists():
+            outpath.mkdir(parents=True, exist_ok=True)
+
+        reg_map_path = outpath / f"{node.inst_name.lower()}_reg_map.py"
+
+        reg_map_tpl = Template(
+            filename=resource_filename("peakrdl_sv", "reg_map.py.tpl"),
+        )
+        with reg_map_path.open("w") as f:
+            f.write(reg_map_tpl.render(block=self.listener.top_node))
